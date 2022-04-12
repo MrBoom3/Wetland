9043e5e18c2feef147e726f63db4a20dffa789c5
MrBoom3 <1080260535@.u.icesi.edu.co>
2022-04-11 21:43:14 -0500

    Version 4.9.1

--- src/model/Dagma.java
+++ src/model/Dagma.java
@@ -10,4 +10,50 @@ public class Dagma{
 	private String mostpopulatedwetland;
 	private String wetlandwherespeciels;
 	
+	
+	public String registerWetland(String name, String ubication, String photourl, boolean type, boolean type, boolean protectedstatus, double size, Date date){
+        
+        boolean continius1=true;
+        String dispatch1="Wetland with a successful registration";
+
+        for( int i=0; (i<MAXWETLANDS) && continuis1; i++){
+            if(wetland[i]==null){
+                wetland[i]=new Wetland(name, ubication, photourl, type, type, protectedstatus, size);
+                continius1=false;
+            }
+        }
+        return dispatch1;
+    }
+	
+	public String registerSpecies(String name, String scientificname, int type, boolean migratorystatus, String name, Date date){
+
+        boolean continius2 = true;
+        String dispatch2="Registered species";
+
+        for(int i=0; (i<MAXSPECIES) && continius2; i++){
+            if (wetland[i]!=null && wetland[i].getname().equalsIgnoreCase(name)){
+                wetland[i].registerSpecies(name,scientificname,type,migratorystatus);
+                continius2=false;
+            }
+        }
+        return dispatch2;
+    }
+	
+	 public String registerEvents(String organizer, int type, String price, String name, Date date){
+        boolean continius3 = true;
+        String dispatch3 = "Registered event";
+
+        for(int i=0; (i<MAXEVENTS) && continius3; i++){
+            if (wetland[i]==null && wetland[i].getname().equalsIgnoreCase(name)){
+                wetland[i].registerEvents(organizer, type, price);
+                continius3=false;
+            }
+        }
+        return dispatch3;
+    }
+	
+	
+	
+	
+	
 }
--- src/ui/Integrative2.java
+++ src/ui/Integrative2.java
@@ -5,7 +5,7 @@ import java.time.LocalDate;
 public class Integrative2{
 
 
-	private static WetlandServices wetlandServices; //??
+	private static WetlandServices wetlandServices; 
 
   public static Scanner reader = new Scanner(System.in);
   public static void main(String[] args){
@@ -24,127 +24,109 @@ public class Integrative2{
         newEvent();
       }
       if((option>3)&&(option<9)){
-        System.out.println("Invalid option");
+        System.out.println("Option invalid");
       }
     }
   }
 
 
-  public static void newWetland(){
-  
-    System.out.println("Put Wetland's name");
-    String WN = reader.nextLine();
-    System.out.println("Put Wetland's location");
-    String WL = reader.nextLine();
-    System.out.println("Is it a private wetland? Put (1) for yes or (2) for no");
-    boolean WP = reader.nextBoolean();
-    String message=wetlandServices.addWetland(WN, WL, WP);
-    System.out.println(message);
-    
+  public static void registerWetland(){
+	
+	int selector = 0;
+	boolean type; boolean zonetype; boolean protectedstatus;
+    System.out.println("Enter Wetland's name");
+    String name = reader.nextLine();
+    System.out.println("Enter Wetland ubication");
+    String ubication = reader.nextLine();
+	System.out.println("Enter url's wetland image");
+    String photourl = reader.nextLine();
+   
+	System.out.println("Enter (1)for a protected status private");
+    selector = Integer.parseInt(reader.nextLine());
+	if (selector==1){
+		protectedstatus=true;
+	}else{
+		protectedstatus=false;
+	}
+	System.out.println("Enter (1) for a rural or (2) for a urban wetland"); 
+    selector = Integer.parseInt(reader.nextLine());
+	if (selector==1){
+		zonetype=true;
+	}else{
+		zonetype=false;
+	}
+	System.out.println("Select wetland type 1 for yes 2 for no");
+	selector=Integer.parseInt(reader.nextLine());
+	if (selector==1){
+		type=true;
+	}else{
+		type=false;
+	}
+	System.out.println("Enter the wetland in km2");
+    double size = reader.nextDouble();
+	System.out.println("Enter the date");
+    System.out.println("day");
+    int day = reader.nextInt();
+    System.out.println("month");
+    int month = reader.nextInt();
+    System.out.println("year");
+    int year = reader.nextInt();
+	
+    Date date = new Date(day, month, year);
+	
     Date();
+	String message=Dagma.createWetland(name, ubication, type);
+    System.out.println(message);
   }
 
 
-  public static void newSpecies(){
+  public static void registerSpecies(){
+	int selector = 0;
+	boolean migratorystatus;
 
-
-    System.out.println("Put the name of the wetland");
-    String WN = reader.nextLine();
-    System.out.println("Put ");
-    System.out.println("Put the species name");
-    String SN = reader.nextLine();
-    System.out.println("Put the species scientific name");
-    String SSN = reader.nextLine();
-    System.out.println("Put the species type");
-    int ST = reader.nextInt();
-    System.out.println("put migratory species");
-    Boolean SM = reader.nextBoolean();
-    System.out.println("Put the wetland you want to add the species to");
-    String SW = reader.nextLine();
-
+    System.out.println("Enter the name of the wetland");
+    String name = reader.nextLine();
+    System.out.println("Enter ");
+    System.out.println("Enter the species name");
+    String name = reader.nextLine();
+    System.out.println("Enter the species scientific name");
+    String scientificname = reader.nextLine();
+    System.out.println("Enter the species type");
+    int type = reader.nextInt();
+    
+    
+	
+	System.out.println("put migratory species");
+	if (selector==1){
+		migratorystatus=true;
+	}else{
+		migratorystatus=false;
+	}
     newWetland();
     Date();
-    wetlandServices.addSpecies(SN, SSN, ST, SM, SW, WN);
+    wetlandServices.addSpecies(name, scientificname, type, migratorystatus, name);
+	
   }
 
 
   public static void newEvent(){
 
-    System.out.println("Put the name of the wetland");
-    String WN = reader.nextLine();
-    System.out.println("Put event organizer's name");
-    String EO = reader.nextLine();
-    System.out.println("Put event's type");
-    String ET = reader.nextLine();
-    System.out.println("Put event's pruice");
-    String EP = reader.nextLine();
-    System.out.println("Put the wetland you want to add the event to");
-    String EW = reader.nextLine();
+    System.out.println("Enter the name of the wetland");
+    String name = reader.nextLine();
+    System.out.println("Enter event organizer's name");
+    String organizer = reader.nextLine();
+    System.out.println("Enter event's type");
+    String type = reader.nextLine();
+    System.out.println("Enter event's price");
+    double price = reader.nextLine();
+    
 
     newWetland();
     Date();
-    wetlandServices.addEvents(EO, ET, EP, EW, WN);
+    wetlandServices.registerEvents(organizer, type, price, name);
   }
 
-  public static void Display(){
-
-    System.out.println"(This is the information for all of our wetlands.");
-    System.out.println("Including total species by type (does not include event information).")
-    
-
-    newWetland();
-  }
-
-
-  public static  void Date(){
-    Scanner keyboard = new Scanner(System.in);
   
-    System.out.println("Put the actual day");
-    int day = keyboard.nextInt();
-    System.out.println("Put the actual month");
-    int month = keyboard.nextInt();
-    System.out.println("Put the actual year");
-    int year = keyboard.nextInt();
-
-    LocalDate date = LocalDate.of(year, month, day);
-  }
-   
-
-  public static int menu(){
-    
-    int option=0;
-
-    System.out.println("Put (1) to create a Wetland.");
-    System.out.println("Put (2) to register a new Species.");
-    System.out.println("Put (3) to register a newevent.");
-
-    System.out.println("Put (5) to see the wetland with less flora.");
-    System.out.println("Put (6) to see wetlands where a species occurs");
-    System.out.println("Put (7) for wetlands and species info.");
-
-    System.out.println("Put (9) to close");
-
-
-    option=reader.nextInt();
-    return option;
-  }
-  public static void maintenance(){
-   System.out.println ("How many maintenance did you do in the" + "year";);
-   Wetland=maintenance;
-   
-  }
-  public static void flora(){
-	  Wetland<flora= new flora;
-	  
-  }
-  public static void speciewetland(){
-	  System.out.printl("Which species do you want to look for in the wetlands?")
-	  species==Wetland;
-	  Wetland= new Wetland1;
-	  
-	  
-	  
-  }
 
 
 	
